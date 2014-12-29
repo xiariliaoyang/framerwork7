c780fafde0721c3dab970fc6352ebd6591bfd634
Jack He <xiariliaoyang@qq.com>
2014-12-29 17:04:21 +0800

    panel

--- script/my-app.js
+++ script/my-app.js
@@ -15,7 +15,7 @@ var mainView = myApp.addView('.view-main', {
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
 myApp.onPageInit('about', function (page) {
   // Do something here for "about" page
-  myApp.alert("进入about页面");
+  myApp.closePanel();
   
 })
 
@@ -29,7 +29,8 @@ ptrContent.on('refresh', function (e) {
     // 模拟2s的加载过程
     setTimeout(function () {
     	
-    	/*myApp.pullToRefreshDone();*/
+    	myApp.pullToRefreshDone();
+
     }, 2000);
 
 });
