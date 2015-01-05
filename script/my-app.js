262a6d3037bddab3dec0cc8a51697f89b9a551d0
Jack He <xiariliaoyang@qq.com>
2015-01-05 14:24:49 +0800

    a

--- script/my-app.js
+++ script/my-app.js
@@ -1,35 +1,45 @@
 // Initialize app and store it to myApp variable for futher access to its methods
 var myApp = new Framework7();
- 
+
 // We need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
- 
+
+
+//add login view
+var mainView = myApp.addView('.view-login', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: false
+});
+
+//登录后
+
+
 // Add view
 var mainView = myApp.addView('.view-main', {
-  // Because we want to use dynamic navbar, we need to enable it for this view:
-  dynamicNavbar: true
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
 });
- 
+
 // Now we need to run the code that will be executed only for About page.
- 
+
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-  // Do something here for "about" page
-  myApp.alert("进入about页面");
-  
+myApp.onPageInit('about', function(page) {
+    // Do something here for "about" page
+    myApp.closePanel();
 })
 
 
 //下拉刷新
 
 var ptrContent = $$('.pull-to-refresh-content');
- 
+
 // 添加'refresh'监听器
-ptrContent.on('refresh', function (e) {
+ptrContent.on('refresh', function(e) {
     // 模拟2s的加载过程
-    setTimeout(function () {
-    	
-    	myApp.pullToRefreshDone();
+    setTimeout(function() {
+
+        myApp.pullToRefreshDone();
+        myApp.alert("刷新完成！","你妹啊");
     }, 2000);
 
 });
