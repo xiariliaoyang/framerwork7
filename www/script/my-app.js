1c074176d2ac664528c70f8b6abfbdf9fff9e44f
Jack He <xiariliaoyang@qq.com>
2015-01-06 14:31:16 +0800

    page query

--- www/script/my-app.js
+++ www/script/my-app.js
@@ -1,40 +1,46 @@
 // Initialize app and store it to myApp variable for futher access to its methods
 var myApp = new Framework7();
- 
+
 // We need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
- 
+
 // Add view
 var mainView = myApp.addView('.view-main', {
-  // Because we want to use dynamic navbar, we need to enable it for this view:
-  dynamicNavbar: true
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
 });
- 
-// Now we need to run the code that will be executed only for About page.
- 
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-  // Do something here for "about" page
-  myApp.closePanel();
-  
+
+//about page
+myApp.onPageInit('about', function(page) {
+    myApp.closePanel();
 })
+
+//box page
+myApp.onPageInit('box', function(page) {
+    var title = page.query.title;
+    //$$(".box-title").html(title);
+    $$.getJSON('http://meiriyiwen.com/json/random', function(data) {
+        $$('.box-title').html(data.title);
+        $$('.box-content').html(data.content);
+    })
+});
 
 
 //下拉刷新
 
 var ptrContent = $$('.pull-to-refresh-content');
- 
+
 // 添加'refresh'监听器
-ptrContent.on('refresh', function (e) {
+ptrContent.on('refresh', function(e) {
     // 模拟2s的加载过程
-    setTimeout(function () {
-    	myApp.pullToRefreshDone();
+    setTimeout(function() {
+        myApp.pullToRefreshDone();
     }, 2000);
 
 });
 
 //登录框
 
-$$(".list-button").on("click",function(){
-	myApp.closeModal(".login-screen");
+$$(".list-button").on("click", function() {
+    myApp.closeModal(".login-screen");
 })
